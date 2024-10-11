2c66fcb8caeaa568f8b80494158d688788114819
DESKTOP-H9VG51M\Adi <jakarta>
2024-10-11 17:02:45 +0700

    feat: button scroll to top part 1

--- app/src/main/java/id/adiyusuf/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/id/adiyusuf/jetheroes/JetHeroesApp.kt
@@ -8,13 +8,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +35,11 @@ import id.adiyusuf.jetheroes.ui.theme.JetHeroesTheme
 @Composable
 fun JetHeroesApp(modifier: Modifier = Modifier) {
     Box(modifier = modifier) {
-        LazyColumn {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton: Boolean by remember { derivedStateOf { listState.firstVisibleItemIndex > 0 } }
+
+        LazyColumn (state = listState) {
             items(HeroesData.heroes, key = { it.id }) { hero ->
                 HeroListItem(
                     name = hero.name,
@@ -67,6 +81,16 @@ fun HeroListItem(
                 .padding(
                     start = 16.dp
                 )
+        )
+    }
+}
+
+@Composable
+fun ScrollToTopButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    FilledIconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = stringResource(R.string.scroll_to_top)
         )
     }
 }
